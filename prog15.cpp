0913ffb1ed1c3dcbf559272ecb30e589861f5e61
EH-GD-MOHIT21 <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2020-09-30 22:23:53 +0530

    Update prog15.cpp

--- prog15.cpp
+++ prog15.cpp
@@ -1,37 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
-
 using namespace std;
 
-void calculater(float num1,float num2, char opr){
+void calculate(float num1,float num2, char opr){
     switch (opr)
     {
     case '+':
-        cout << "Sum is : " <<num1+num2;   
+        cout << "Sum is :-> " <<num1+num2;   
         // checking condition for sum
         break;
     case '-':
-        cout << "Diffrence is : " << num1-num2;  
+        cout << "Diffrence is :-> " << num1-num2;  
         //checking condition for subtract.
         break;
     case '*':
-        cout << "Multiplication is : " << num1*num2;
+        cout << "Multiplication is :-> " << num1*num2;
         //checking condition for multiply.
         break;
     case '/':
         if(num2==0){
-            cout << "Denominator can't be zero.";
+            cout << "OOPS :-< Denominator can't be zero.";
             // checking whether denominator is zero or not.
         }
         else
             // if denominator is not zero then calculating division.
-            cout << "Division is : " << num1 / num2 ;
+            cout << "Division is :-> " << num1 / num2 ;
         break;
     
     default:
             //if none of above is correct case.
-            cout << "data entered is not correct." ;
+            cout << "data entered is incorrect:-<" ;
     }
 
 }
@@ -45,6 +44,6 @@ int main(){
     cin >> num2;   // taking second number as user input.
     cout << "Enter operation +,-,/,* here ->: ";
     cin >> opr;    // taking operation as user input(+,-,*,/).
-    calculater(num1,num2,opr);   // calling our function with sending copies of values.
+    calculate(num1,num2,opr);   // calling our function calculate with sending copies of values of num1,num2 and the operator to be performed.
     return 0;
 }
